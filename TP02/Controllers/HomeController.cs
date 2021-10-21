0d9354fce08bda396129d512fa929bcc6cd39468
Fellipe Soares <fe.soaresromano@gmail.com>
2021-10-21 20:52:38 -0300

    upload

---
+++ TP02/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿//FELLIPE SOARES ROMANO CB3005411
+//STEFANY LISBOA DA SILVA CB3005551
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TP02.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
